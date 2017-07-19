5792ee6620b434bc264797721adc4f1d97bea22f
Jeff Escalante <jescalan@users.noreply.github.com>
2017-07-19 18:47:01 -0400

    Fix a bug with minifying scripts that contain locals (#21)

--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@ const {readFileSync} = require('fs')
 const test = require('ava')
 const fixtures = path.join(__dirname, 'fixtures')
 const exp = require('reshape-expressions')
+const evalCode = require('reshape-eval-code')
 
 test('aggressively collapses whitespace', (t) => {
   return compare(t, 'whitespace_aggressive', { aggressiveCollapse: true })
@@ -26,15 +27,29 @@ test('removes redundant attributes', (t) => compare(t, 'redundant_attr'))
 test('removes comments', (t) => compare(t, 'comments'))
 test('handles custom tags', (t) => compare(t, 'custom_tag'))
 test('handles expressions in tags', (t) => compare(t, 'expressions_in_tags'))
+test('minifies scripts that contain locals', (t) => compare(t, 'script_locals'))
+
+test('doesn\'t minify scripts that have active expressions', (t) => {
+  const name = 'script_expressions'
+  const input = readFileSync(path.join(fixtures, `${name}.html`), 'utf8')
+  const expected = readFileSync(path.join(fixtures, `${name}.expected.html`), 'utf8')
+
+  return reshape({ plugins: [exp(), minify()] })
+    .process(input)
+    .then((res) => {
+      t.is(res.output({ testContent: 'thisIsContent' }).trim(), expected.trim())
+    })
+})
 
 function compare (t, name, opts, log) {
   const input = readFileSync(path.join(fixtures, `${name}.html`), 'utf8')
   const expected = readFileSync(path.join(fixtures, `${name}.expected.html`), 'utf8')
+  const locals = { testId: 'thisIsId', testClass: 'thisIsClass', testContent: 'thisIsContent' }
 
-  return reshape({ plugins: [ exp(), minify(opts) ] })
+  return reshape({ plugins: [ exp(), evalCode(locals), minify(opts) ] })
     .process(input)
-    .tap((res) => log && console.log(res.output({ testId: 'thisIsId', testClass: 'thisIsClass', testContent: 'thisIsContent' })))
+    .tap((res) => log && console.log(res.output()))
     .then((res) => {
-      t.is(res.output({ testId: 'thisIsId', testClass: 'thisIsClass', testContent: 'thisIsContent' }).trim(), expected.trim())
+      t.is(res.output().trim(), expected.trim())
     })
 }
